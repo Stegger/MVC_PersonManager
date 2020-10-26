9f13f2f5ea5070fabd98ed5e3556ffd8d1493367
Peter Stegger <stegger.peter@gmail.com>
2020-10-26 09:46:42 +0100

    Added getters for all students and teachers to the PersonManager class.

--- src/personmanager/bll/PersonManager.java
+++ src/personmanager/bll/PersonManager.java
@@ -1,6 +1,7 @@
 package personmanager.bll;
 
 import personmanager.be.Person;
+import personmanager.be.Student;
 import personmanager.be.Teacher;
 
 import java.util.ArrayList;
@@ -22,16 +23,34 @@ public class PersonManager {
         return allPersons;
     }
 
-    public List<Teacher> getAllTeachers() {
+    public List<Teacher> getAllTeachers()
+    {
         List<Teacher> allTeachers = new ArrayList<>();
 
-        for (Person person : allPersons) {
-            if (person instanceof Teacher) {
+        for (Person person : allPersons)
+        {
+            if (person instanceof Teacher)
+            {
                 Teacher teacher = (Teacher) person;
                 allTeachers.add(teacher);
             }
         }
+
         return allTeachers;
+    }
+
+    public List<Student> getAllStudents()
+    {
+        List<Student> students = new ArrayList<>();
+        for(Person person : allPersons)
+        {
+            if(person instanceof Student)
+            {
+                Student student = (Student) person;
+                students.add(student);
+            }
+        }
+        return students;
     }
 
 
