670c32a17f8170eff586d77be385e525e0e4b399
Peter Stegger <stegger.peter@gmail.com>
2020-10-26 09:34:24 +0100

    Finished Student and Person (Exercise 4)

--- src/personmanager/be/Student.java
+++ src/personmanager/be/Student.java
@@ -1,28 +1,53 @@
 package personmanager.be;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Student extends Person {
+
     private String education;
     private List<GradeInfo> gradeReport;
 
     public Student(int id, String name, String email, String education) {
         super(id, name, email);
         this.education = education;
+        gradeReport = new ArrayList<>();
     }
 
     public String getEducation() {
         return education;
     }
 
+    /**
+     *
+     * @param subject
+     * @return The grade matching the subject if a subject is found, otherwise -1.
+     */
     public int getGrade(String subject) {
-        //TODO
-        return 0;
+
+        for (GradeInfo gradeInfo: gradeReport)
+        {
+            if(gradeInfo.getSubject().equalsIgnoreCase(subject))
+            {
+                return gradeInfo.getGrade();
+            }
+        }
+        return -1;
     }
 
-    public double getAverageGrade() {
-        //TODO
-        return 0.0;
+    /**
+     * Returns the students object average.
+     * @return
+     */
+    public double getAverageGrade()
+    {
+        double sum = 0.0;
+        for(GradeInfo gradeInfo : gradeReport)
+        {
+            sum += gradeInfo.getGrade();
+        }
+        double avg = sum / gradeReport.size();
+        return avg;
     }
 
     public List<GradeInfo> getGradeReport() {
--- src/personmanager/gui/console/Console.java
+++ src/personmanager/gui/console/Console.java
@@ -1,6 +1,8 @@
 package personmanager.gui.console;
 
+import personmanager.be.GradeInfo;
 import personmanager.be.Person;
+import personmanager.be.Student;
 import personmanager.be.Teacher;
 import personmanager.bll.PersonManager;
 import personmanager.gui.javafx.Main;
@@ -12,9 +14,17 @@ public class Console {
 
     public static void main(String[] args)
     {
+        Student student = new Student(1, "Peter", "peter@easv365.dk", "DMU");
 
-        MainMenu mainMenu = new MainMenu();
-        mainMenu.run();
+        student.addGrade(new GradeInfo("SCO", 12));
+        student.addGrade(new GradeInfo("SDE", 4));
+        student.addGrade(new GradeInfo("ITO", 10));
+
+        System.out.println("Average: " + student.getAverageGrade());
+
+
+        //MainMenu mainMenu = new MainMenu();
+        //mainMenu.run();
 
     }
 
